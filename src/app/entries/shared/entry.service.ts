abb6bab39b0c093d60201a4347f3cbea6a789bf7
Alan Mills <aelonrench@yahoo.com>
2018-04-10 18:56:34 -0400

    Integrate larger faux backend Server file with larger quantity content.

--- src/app/entries/shared/entry.service.ts
+++ src/app/entries/shared/entry.service.ts
@@ -8,7 +8,7 @@ export class EntryService {
 
     }
     getEntries(): Promise<Entry[]> {
-        return this.http.get('/app/enties')
+        return this.http.get('/app/entries')
         .toPromise()
         .then(response => response.json().data as Entry[]);
     }
